c6c0819763a4068378cc5d3ba49de6ede1316a9f
Rasmus Gustafsson <rasmus.gustafsson@mail.com>
2020-11-18 17:34:28 +0200

    Made tiles unwalkable by default. Useful for dungeon generation

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -7,14 +7,8 @@ class GameMap:
         self.tiles = self.initialize_tiles()
 
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-
-        tiles[30][22].blocked = True
-        tiles[30][22].blocked_sight = True
-        tiles[31][22].blocked = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].blocked = True
-        tiles[32][22].block_sight = True
+        # True makes all tiles by default UNWALKABLE
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
 
