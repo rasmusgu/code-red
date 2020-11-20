7a472c3cf68152ef724e4d672e79d8f36861b420
Rasmus Gustafsson <rasmus.gustafsson@mail.com>
2020-11-20 12:42:53 +0200

    Added blocking function to entity.py

--- entity.py
+++ entity.py
@@ -15,3 +15,10 @@ class Entity:
         # Move the entity by a given amount
         self.x += dx
         self.y += dy
+
+# NOTE: not in the class because it relates to all entities but no specific entity
+def get_blocking_entities_at_location(entities, destination_x, destination_y):
+    for entity in entities:
+        if entity.blocks and entity.x == destination_x and entity.y == destination_y:
+            return entity
+    return None
