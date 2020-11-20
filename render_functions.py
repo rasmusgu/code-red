b08fa1ec54f97bc869e084c2e482dad0ea61a985
Rasmus Gustafsson <rasmus.gustafsson@mail.com>
2020-11-20 16:31:23 +0200

    made alive entities appear above corpses

--- render_functions.py
+++ render_functions.py
@@ -27,9 +27,11 @@ def render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_w
                         libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
                     else:
                         libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+    
+    entities_in_render_order = sorted(entities, key=lambda x: x.render_order.value)
 
     # Renders all listed entities
-    for entity in entities:
+    for entity in entities_in_render_order:
         draw_entity(con, entity, fov_map)
     
     libtcod.console_set_default_foreground(con, libtcod.white)
