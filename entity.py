168b0ecb1b1ac37818469c8d9765d470a0c0c6b4
Rasmus Gustafsson <rasmus.gustafsson@mail.com>
2020-11-20 12:36:39 +0200

    added name and block variables to entity

--- entity.py
+++ entity.py
@@ -2,11 +2,14 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-    def __init__(self, x, y, char, color):
+    def __init__(self, x, y, char, color, name, blocks=False):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
+        self.name = name
+        # if blocks isn't passed on initialisation it's False by default
+        self.blocks = blocks
 
     def move(self, dx, dy):
         # Move the entity by a given amount
