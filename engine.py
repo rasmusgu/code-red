9ab41cde74a7f12607cba0ed7fb5e72d745a32b3
Rasmus Gustafsson <rasmus.gustafsson@mail.com>
2020-11-20 10:07:58 +0200

    Added light_wall and _ground colors to engine.py

--- engine.py
+++ engine.py
@@ -31,6 +31,8 @@ def main():
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150)
+        'light_wall': libtcod.Color(130, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
     }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
