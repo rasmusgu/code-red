b4a0d76ee0c13bf6e35a69ce4ea3579f0131da41
Rasmus Gustafsson <rasmus.gustafsson@mail.com>
2020-11-20 10:08:34 +0200

    Fixed typo

--- engine.py
+++ engine.py
@@ -30,7 +30,7 @@ def main():
     # Colors
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150)
+        'dark_ground': libtcod.Color(50, 50, 150),
         'light_wall': libtcod.Color(130, 110, 50),
         'light_ground': libtcod.Color(200, 180, 50)
     }
