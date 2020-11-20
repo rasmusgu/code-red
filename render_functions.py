33cfac448966a44ebc3e95979f2e439e62780bdb
Rasmus Gustafsson <rasmus.gustafsson@mail.com>
2020-11-20 10:29:56 +0200

    Made entities outside of FOV not visible

--- render_functions.py
+++ render_functions.py
@@ -22,7 +22,7 @@ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, sc
 
     # Renders all listed entities
     for entity in entities:
-        draw_entity(con, entity)
+        draw_entity(con, entity, fov_map)
     
     # ???
     libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
@@ -32,10 +32,12 @@ def clear_all(con, entities):
     for entity in entities:
         clear_entity(con, entity)
 
-def draw_entity(con, entity):
-    # Draws entity
-    libtcod.console_set_default_foreground(con, entity.color)
-    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+def draw_entity(con, entity, fov_map):
+    # Checks for FOV
+    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        # Draws entity
+        libtcod.console_set_default_foreground(con, entity.color)
+        libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
 
 def clear_entity(con, entity):
     # Erases entity
